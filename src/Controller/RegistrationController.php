5f3471509c0c6e4252f9ee521fa538c64173bf0a
heinzwalace <heinzwalace@gmail.com>
2024-06-08 12:08:37 +0200

    config mail

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -46,8 +46,9 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('testmailer@booster.com', 'testmailer'))
-                    ->to($user->getEmail())
+                    // ->from(new Address('testmailer@booster.com', 'testmailer'))
+                    ->from('heinzcodeur@gmx.com')
+                    ->to('heinzcodeur@gmx.com')
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
